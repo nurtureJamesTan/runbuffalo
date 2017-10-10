763d23d789f718e36eb8ffa17fa7bcf725716f3e
James <james@mercstudio.com>
2017-10-10 15:54:33 +0800

    log to environment

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "runbuffalo v1.0.6"
+echo "runbuffalo v1.0.7"
 
 export APPDIR=/home/app/web
 cd $APPDIR
@@ -12,8 +12,8 @@ fi;
 echo "preparing tmp..."
 gosu app mkdir -p $APPDIR/tmp
 echo "migration & seeds..."
-gosu app bin/heroku migrate
-gosu app bin/heroku t db:seed
+gosu app bin/heroku migrate > log/$GOENV.log
+gosu app bin/heroku t db:seed > log/$GOENV.log
 # pkill heroku
-echo "Starting up..."
-gosu app $@
+echo "Starting up $GOENV..."
+gosu app $@ > log/$GOENV.log
