1a84864659dbdd97a5b1a2b1fab7af7945e1e652
James <james@mercstudio.com>
2018-04-03 15:04:33 +0800

    run bg in staging, minus cron

--- entry.sh
+++ entry.sh
@@ -34,11 +34,15 @@ if [ "$MIGRATE" -eq 1 ]; then
 fi
 
 if [ "$CRON" -eq 1 ]; then
-	echo "4:CRON $GO_ENV..."
-	cat /home/app/web/cron_task.sh
-
-	cp -rf /home/app/web/cron_task.sh /var/spool/cron/crontabs/root
-	crond -l 2 -f
+	if [ "$GO_ENV" = "production" ]; then
+		echo "4:CRON $GO_ENV..."
+		cat /home/app/web/cron_task.sh
+		cp -rf /home/app/web/cron_task.sh /var/spool/cron/crontabs/root
+		echo "starting crond..."
+		crond -l 2
+	fi
+	echo "starting twitter:update"
+	bin/heroku t twitter:update
 else
 	# pkill heroku
 	echo "4:Runing $GO_ENV: $@, UID $UID"
